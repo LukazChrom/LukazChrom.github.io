2ac942bd4125f5fdfa35364b24bcb5e98e9aaca7
Lukas <104063917+LukazChrom@users.noreply.github.com>
2022-06-02 17:15:25 +0200

    Add files via upload

---
+++ FolderJS/CodeJS.js
@@ -0,0 +1,42 @@
+const SP = "!$!WEBpast421";
+
+//alert("SSL: " + sessionStorage.length);
+
+var varP;
+
+var denialCounter = sessionStorage.length;
+
+if(denialCounter == 0){
+	varP = prompt("Welcome to L's 100%-self programmed Prototype-Webpage, please enter the password", "");
+	sessionStorage.setItem('UserPassword', varP);
+}
+
+if(sessionStorage.getItem('UserPassword') == SP && denialCounter == 0) {
+	alert("Passwort richtig :D");
+}
+
+
+if(sessionStorage.getItem('UserPassword') == SP) {
+
+	var YTlink = document.getElementById("YT-Channel");
+	YTlink.innerHTML = "YT-Channel me <]";
+	YTlink.style.display = "inline";
+
+	YTlink.style.color = "#0e274f";							//Waere richtig aetzend hier jetzt anders vorzugehgen XD
+	YTlink.style.background = "#0e274f";	           	 	//Also einfach die Farben nutzen :) Ist ja hier nicht sichherheitsrelevant <D
+
+	YTlink.addEventListener("mouseenter", function() {
+		YTlink.style.color = "white";
+	YTlink.style.background = "red";
+	});
+	YTlink.addEventListener("mouseleave", function() {
+		YTlink.style.color = "#0e274f";
+	YTlink.style.background = "#0e274f";
+	});
+
+
+}else if(denialCounter == 0) {
+	alert("Passwort nicht richtig eingegeben :(");
+}
+
+//alert("Die Eingabe: " + sessionStorage.getItem('UserPassword') + " _ SSL: " + sessionStorage.length);
